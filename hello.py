0d7463abbfa9094faca81b89f5c962d0d6b6e3cf
spitzc <33139308+spitzc@users.noreply.github.com>
2017-10-26 20:46:19 -0400

    add first file
    yay

---
+++ hello.py
@@ -0,0 +1,3 @@
+all_classes = ['Human-Information Behavior','Research Mthds/Law Lit','Strategic Leadership','Conservation and Preservation','Info Services & Resources', 'Information Arch/Inter Design','Information Professions','Acad Libraries and Scholarly','Usability Theory & Practice','Mgmt of Archives/Sp Collection','Government Info Sources','Library Media Centers','Mgmt of Archives/Sp Collection','Information Science Research']
+
+ print(all_classes)
